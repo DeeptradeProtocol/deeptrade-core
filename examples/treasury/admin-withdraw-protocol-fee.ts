6405337901663c66d0d7f5afbf37e50160d52c65
avernikoz <avernikoz@gmail.com>
2025-07-25 18:44:55 +0300

    upd examples for withdrwing coverage fee and protocol fee

--- examples/treasury/admin-withdraw-protocol-fee.ts
+++ examples/treasury/admin-withdraw-protocol-fee.ts
@@ -1,34 +1,34 @@
-import { ADMIN_CAP_OBJECT_ID, DEEP_COIN_TYPE, SUI_COIN_TYPE, DEEPTRADE_CORE_PACKAGE_ID } from "../constants";
+import { ADMIN_CAP_OBJECT_ID, DEEPTRADE_CORE_PACKAGE_ID } from "../constants";
 import { MULTISIG_CONFIG } from "../multisig/multisig";
 import { buildAndLogMultisigTransaction } from "../multisig/buildAndLogMultisigTransaction";
 import { getWithdrawFeeTx } from "./getWithdrawFeeTx";
+import { Transaction } from "@mysten/sui/transactions";
+import { getTreasuryBags } from "./utils/getWrapperBags";
+import { processFeesBag } from "./utils/processFeeBag";
 
 // yarn ts-node examples/treasury/admin-withdraw-protocol-fee.ts > admin-withdraw-protocol-fee.log 2>&1
 (async () => {
-  console.warn(`Building transaction to withdraw protocol fees for SUI and DEEP`);
+  console.warn(`Building transaction to withdraw all protocol fees`);
+  const tx = new Transaction();
 
-  // Withdraw SUI protocol fee
-  const tx = getWithdrawFeeTx({
-    coinType: SUI_COIN_TYPE,
-    target: `${DEEPTRADE_CORE_PACKAGE_ID}::treasury::withdraw_protocol_fee`,
-    user: MULTISIG_CONFIG.address,
-    adminCapId: ADMIN_CAP_OBJECT_ID,
-    pks: MULTISIG_CONFIG.publicKeysSuiBytes,
-    weights: MULTISIG_CONFIG.weights,
-    threshold: MULTISIG_CONFIG.threshold,
-  });
+  const { protocolFeesBagId } = await getTreasuryBags();
 
-  // Withdraw DEEP protocol fee (pool creation fee)
-  getWithdrawFeeTx({
-    coinType: DEEP_COIN_TYPE,
-    target: `${DEEPTRADE_CORE_PACKAGE_ID}::treasury::withdraw_protocol_fee`,
-    user: MULTISIG_CONFIG.address,
-    adminCapId: ADMIN_CAP_OBJECT_ID,
-    transaction: tx,
-    pks: MULTISIG_CONFIG.publicKeysSuiBytes,
-    weights: MULTISIG_CONFIG.weights,
-    threshold: MULTISIG_CONFIG.threshold,
-  });
+  // Process coverage fees
+  const { coinsMapByCoinType } = await processFeesBag(protocolFeesBagId);
+  const coinTypes = Object.keys(coinsMapByCoinType);
+
+  for (const coinType of coinTypes) {
+    getWithdrawFeeTx({
+      coinType: coinType,
+      target: `${DEEPTRADE_CORE_PACKAGE_ID}::treasury::withdraw_protocol_fee`,
+      user: MULTISIG_CONFIG.address,
+      adminCapId: ADMIN_CAP_OBJECT_ID,
+      transaction: tx,
+      pks: MULTISIG_CONFIG.publicKeysSuiBytes,
+      weights: MULTISIG_CONFIG.weights,
+      threshold: MULTISIG_CONFIG.threshold,
+    });
+  }
 
   await buildAndLogMultisigTransaction(tx);
 })();
