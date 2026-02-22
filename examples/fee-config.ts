a25249b82af60c0f6666dc281d7738f2eb6656d5
avernikoz <avernikoz@gmail.com>
2026-02-22 15:18:15 +0300

    set up fee config

--- examples/fee-config.ts
+++ examples/fee-config.ts
@@ -1,4 +1,4 @@
-import { DEEP_COIN_TYPE, SUI_COIN_TYPE } from "./constants";
+import { DEEP_COIN_TYPE, SUI_COIN_TYPE, USDC_COIN_TYPE, WAL_COIN_TYPE } from "./constants";
 
 /**
  * Converts a human-readable percentage into the raw format expected by the contract.
@@ -30,9 +30,9 @@ export function percentToRaw(percent: number): number {
 // ---------------------------------------------------------
 export const defaultFeesConfig = {
   deepFeeTypeTakerRate: percentToRaw(0.06), // 6 bps = 600_000 raw
-  deepFeeTypeMakerRate: percentToRaw(0.03), // 3 bps = 300_000 raw
+  deepFeeTypeMakerRate: percentToRaw(0.04), // 3 bps = 300_000 raw
   inputCoinFeeTypeTakerRate: percentToRaw(0.05), // 5 bps = 500_000 raw
-  inputCoinFeeTypeMakerRate: percentToRaw(0.02), // 2 bps = 200_000 raw
+  inputCoinFeeTypeMakerRate: percentToRaw(0.03), // 2 bps = 200_000 raw
   maxDeepFeeCoverageDiscountRate: percentToRaw(25), // 25% = 250_000_000 raw
 };
 
@@ -41,15 +41,98 @@ export const defaultFeesConfig = {
 // ---------------------------------------------------------
 export const poolSpecificFeesConfig = [
   {
-    // DEEP_SUI pool id
+    // DEEP_USDC
+    poolId: "0xde096bb2c59538a25c89229127fe0bc8b63ecdbe52a3693099cc40a1d8a2cfd4",
+    baseCoin: DEEP_COIN_TYPE,
+    quoteCoin: USDC_COIN_TYPE,
+    fees: {
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.1),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.1),
+      // Discount
+      maxDeepFeeCoverageDiscountRate: percentToRaw(25),
+    },
+  },
+  {
+    // DEEP_SUI
     poolId: "0xb663828d6217467c8a1838a03793da896cbe745b150ebd57d82f814ca579fc22",
     baseCoin: DEEP_COIN_TYPE,
     quoteCoin: SUI_COIN_TYPE,
     fees: {
-      deepFeeTypeTakerRate: percentToRaw(0.06),
-      deepFeeTypeMakerRate: percentToRaw(0.03),
-      inputCoinFeeTypeTakerRate: percentToRaw(0.05),
-      inputCoinFeeTypeMakerRate: percentToRaw(0.02),
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.1),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.1),
+      // Discount
+      maxDeepFeeCoverageDiscountRate: percentToRaw(25),
+    },
+  },
+  {
+    // SUI_USDC
+    poolId: "0xe05dafb5133bcffb8d59f4e12465dc0e9faeaa05e3e342a08fe135800e3e4407",
+    baseCoin: SUI_COIN_TYPE,
+    quoteCoin: USDC_COIN_TYPE,
+    fees: {
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.1),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.09),
+      // Discount
+      maxDeepFeeCoverageDiscountRate: percentToRaw(25),
+    },
+  },
+  {
+    // WAL_USDC
+    poolId: "0x56a1c985c1f1123181d6b881714793689321ba24301b3585eec427436eb1c76d",
+    baseCoin: WAL_COIN_TYPE,
+    quoteCoin: USDC_COIN_TYPE,
+    fees: {
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.08),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.075),
+      // Discount
+      maxDeepFeeCoverageDiscountRate: percentToRaw(25),
+    },
+  },
+  {
+    // XBTC_USDC
+    poolId: "0x20b9a3ec7a02d4f344aa1ebc5774b7b0ccafa9a5d76230662fdc0300bb215307",
+    baseCoin: "0x876a4b7bce8aeaef60464c11f4026903e9afacab79b9b142686158aa86560b50::xbtc::XBTC",
+    quoteCoin: USDC_COIN_TYPE,
+    fees: {
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.08),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.075),
+      // Discount
+      maxDeepFeeCoverageDiscountRate: percentToRaw(25),
+    },
+  },
+  {
+    // WBTC_USDC
+    poolId: "0xf5142aafa24866107df628bf92d0358c7da6acc46c2f10951690fd2b8570f117",
+    baseCoin: "0x0041f9f9344cac094454cd574e333c4fdb132d7bcc9379bcd4aab485b2a63942::wbtc::WBTC",
+    quoteCoin: USDC_COIN_TYPE,
+    fees: {
+      // Maker fees
+      deepFeeTypeMakerRate: percentToRaw(0.06),
+      inputCoinFeeTypeMakerRate: percentToRaw(0.06),
+      // Taker fees
+      deepFeeTypeTakerRate: percentToRaw(0.08),
+      inputCoinFeeTypeTakerRate: percentToRaw(0.075),
+      // Discount
       maxDeepFeeCoverageDiscountRate: percentToRaw(25),
     },
   },
